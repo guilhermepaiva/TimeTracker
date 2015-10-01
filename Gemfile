3562b4d01475ae70a6cd65bb4a55c8d3bda2b749
guilhermepaiva <guilhermepaiva9@gmail.com>
2015-10-01 20:58:51 -0300

    subdomínios finalizado

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'rails', '4.2.4'
-gem 'sqlite3'
+#gem 'sqlite3'
+gem 'pg'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
@@ -15,6 +16,8 @@ gem 'bootstrap-sass', '~> 3.3.5'
 
 gem 'devise'
 
+gem 'apartment'
+
 
 group :development, :test do
   gem 'byebug'
@@ -26,6 +29,7 @@ group :development, :test do
   gem 'factory_girl_rails'
   gem 'database_cleaner'
   gem 'shoulda-matchers'
+  gem 'pg'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,25 @@
+class SubdomainPresent
+  def self.matches?(request)
+    request.subdomain.present?
+  end
+end
+
+class SubdomainBlank
+  def self.matches?(request)
+    request.subdomain.blank?
+  end
+end
+
 Rails.application.routes.draw do
-  devise_for :users
-  get 'accounts/new'
 
-  get 'accounts/create'
+  constraints(SubdomainPresent) do
+    devise_for :users
+    root 'projects#index', as: :subdomain_root
+  end
 
-  get 'welcome/index'
+  constraints(SubdomainBlank) do
+    resources :accounts, only: [:new, :create]
+    root 'welcome#index'
+  end
 
-  root "welcome#index"
-
-  resources :accounts
 end
